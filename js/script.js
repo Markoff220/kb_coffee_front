8be80bd7b1a969523e54d170f273009d6e1105a4
Markoff220 <109596551+Markoff220@users.noreply.github.com>
2024-04-01 11:38:26 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,30 +1,53 @@
- let tg = window.Telegram.WebApp;
- tg.expand();
+let tg = window.Telegram.WebApp;
+tg.expand();
 
 
 // Функция отправки данных в ТГ | Отправки кода подтверждения на почту
 document.addEventListener('DOMContentLoaded', function() {
     // Отправка кода на почту
     document.getElementById('send_code').addEventListener('click', function() {
-        var email = document.getElementById('email').value;
+        var fields = ['email', 'name', 'username', 'department'];
+        var errStatus = 0;
 
-        if (!email.includes('strelka')) {
-            document.getElementById('emailError').style.display = 'block';
-            return;
-            };
+        fields.forEach(function(field) {
+            var value = document.getElementById(field).value;
+            var errorElement = document.getElementById(field + 'Error');
 
-        // Данные для отправки
-        let data = {
-            'type': 'check',
-            'name': document.getElementById('name').value,
-            'username': document.getElementById('username').value,
-            'department': document.getElementById('department').value,
-            "email": email,
-        };
+            if (field === 'email') {
+                var atIndex = value.indexOf('@');
 
-        tg.sendData(JSON.stringify(data));
+                if (atIndex !== -1) {
+                    var domain = value.slice(atIndex + 1);
+                    if (!domain.includes('strelka')) {
+                        errorElement.style.display = 'block';
+                        errStatus = 1;
+                        return; // Прерываем выполнение текущей итерации
+                    }
+                }
+                else {
+                    errorElement.style.display = 'block';
+                    errStatus = 1;
+                }
+
+            } else if (value === '') {
+                errorElement.style.display = 'block';
+                errStatus = 1;
+            } else {
+                errorElement.style.display = 'none';
+            }
         });
 
-
+        if (errStatus === 0) {
+            // Данные для отправки
+            let data = {
+                'type': 'check',
+                'name': document.getElementById('name').value,
+                'username': document.getElementById('username').value,
+                'department': document.getElementById('department').value,
+                "email": document.getElementById('email').value,
+            };
+            alert('eee');
+            tg.sendData(JSON.stringify(data));
+        }
     });
-
+});
